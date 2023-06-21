4e9490c44d2960779509a0f06400df6782c5de12
pankaj-kb <pankajb2601@gmail.com>
2023-06-21 16:25:17 +0530

    starting with app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "react-toastify/dist/ReactToastify.css"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
