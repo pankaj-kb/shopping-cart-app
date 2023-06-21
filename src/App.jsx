15fed08ee78ac0e329c88bf20dc831e19ad282da
pankaj-kb <pankajb2601@gmail.com>
2023-06-21 17:12:03 +0530

    done till add to cart

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,28 @@ import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "react-toastify/dist/ReactToastify.css"
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
+  const [cartItem, setCartItem] = useState([]);
+
+  const addtoCart = item => {
+    
+    const isExist = cartItem.findIndex(function(array){
+      return array.id === item.id
+    })
+
+    if (isExist !== -1) {
+      toast("already added in cart", {type: "error"})
+    }
+
+    setCartItem([...cartItem, item])
+  }
+
   return (
     <>
+    <h1>Hello, Pankaj</h1>
     </>
   )
 }
